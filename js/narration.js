8665d09a401f22570b21b15c41b5feaabd304546
Emanuele Ghebaur <emanueleghebaur@gmail.com>
2021-12-07 23:18:26 +0100

    code fix && comments

--- js/narration.js
+++ js/narration.js
@@ -1,58 +1,43 @@
-let lines = function(l) {
+let lines = function (l) {
+    
+    let xyVertex = []; //Array that will contain the vertexes of the polyLine
+    let videoList = document.getElementsByClassName("wrapper"); //Array to obtain all the div.wrapper elements
+    
+    l.setup = function () {
+        var canvasL = l.createCanvas(l.windowWidth, l.windowHeight);
+        canvasL.parent("#canvasLines");
+    };
 
-// let videos = []; // Array that contains the videos
-// let vidPos = [];
-let xyVertex = [];
-let videoList = document.getElementsByClassName("wrapper");
-// for(let j = 0; j < videoList.length; j++) {
-//     vidPos.push(
-//         videoList[j].getBoundingClientRect()
-//     );
-// }
+    l.draw = function () {
+        const canvas = document.querySelector("#canvasLines");
+        const ctx = this.canvas.getContext("2d");
 
-// for (let j = 0; j < videoList.length; j++) {
-//     videos.push({
-//     x: vidPos[j].right - (videoList[j].offsetWidth/2),
-//     y: vidPos[j].top + (videoList[j].offsetHeight/2)
-//     });
-// }
+        l.background("black"); //to avoid trails
 
-l.setup = function() {
-    var canvasL = l.createCanvas(l.windowWidth, l.windowHeight);
-    canvasL.parent('#canvasLines');
-};
+        l.noFill();
+        l.stroke("red");
+        l.strokeWeight(2);
 
-l.draw = function() {
-    const canvas = document.querySelector('#canvasLines');
-    const ctx = this.canvas.getContext('2d');
-
-    // var linesarray = [];
-    l.background("black");
-
-    l.noFill();
-    l.stroke("red");
-    l.strokeWeight(2);
-
-    videoList.forEach(function(item, index) {
-        videoList[index].firstElementChild.addEventListener("mouseover", function(){
-            // console.log(videos[index].x,videos[index].y);
-            xyVertex.push({
-                x: videoList[index].firstElementChild.getBoundingClientRect().left+((videoList[index].offsetHeight)/4),
-                y: videoList[index].firstElementChild.getBoundingClientRect().top-videoList[index].parentNode.getBoundingClientRect().top+(videoList[index].firstElementChild.offsetHeight/2)
+        videoList.forEach(function (item, index) {
+            //when :hover the video
+            videoList[index].firstElementChild.addEventListener("mouseover", function () {
+                //add the center of the video as a new vertex
+                xyVertex.push({
+                    x: videoList[index].firstElementChild.getBoundingClientRect().left + videoList[index].offsetWidth / 2,
+                    y: videoList[index].firstElementChild.getBoundingClientRect().top - videoList[index].parentNode.getBoundingClientRect().top + videoList[index].firstElementChild.offsetHeight / 2,
+                });
+                // console.log(videos[index].x,videos[index].y);
             });
         });
-    });
 
-    l.beginShape();
-    xyVertex.forEach(function(item, index) {
-        l.vertex(xyVertex[index].x, xyVertex[index].y);
-    });
-    l.vertex(l.mouseX, l.mouseY);
-    l.endShape();
-
-
+        //draw the polyLine
+        l.beginShape();
+        xyVertex.forEach(function (item, index) {
+            l.vertex(xyVertex[index].x, xyVertex[index].y);
+        });
+        l.vertex(l.mouseX, l.mouseY);
+        l.endShape();
+    };
 };
-
-}
 
 let canvasLines = new p5(lines);

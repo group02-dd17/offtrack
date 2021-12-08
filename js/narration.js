b7cf790843eb1618acedd61974e67ed15e7afa66
Emanuele Ghebaur <emanueleghebaur@gmail.com>
2021-12-09 00:41:05 +0100

    prova laggosa canvas allungato

--- js/narration.js
+++ js/narration.js
@@ -1,10 +1,14 @@
 let lines = function (l) {
-    
+
     let xyVertex = []; //Array that will contain the vertexes of the polyLine
     let videoList = document.getElementsByClassName("wrapper"); //Array to obtain all the div.wrapper elements
-    
+
     l.setup = function () {
-        var canvasL = l.createCanvas(l.windowWidth, l.windowHeight);
+        var canvasL = l.createCanvas(l.windowWidth, document.getElementById("container").offsetHeight);
+        
+        console.log(l.windowWidth);
+        console.log(document.getElementById("container").offsetHeight);
+
         canvasL.parent("#canvasLines");
     };
 
@@ -38,6 +42,17 @@ let lines = function (l) {
         l.vertex(l.mouseX, l.mouseY);
         l.endShape();
     };
+    // l.windowResized=function() {
+    //     const css = getComputedStyle(l.canvas.parentElement),
+    //           marginWidth = round(float(css.marginLeft) + float(css.marginRight)),
+    //           marginHeight = round(float(css.marginTop) + float(css.marginBottom)),
+    //           w = l.windowWidth - marginWidth, h = l.windowHeight - marginHeight;
+      
+    //     resizeCanvas(w, h, true);
+    //   }
 };
 
+
+
 let canvasLines = new p5(lines);
+

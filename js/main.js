3edf54bcc1ec41ba3d176435b82d212bd7a69f22
AndreaDesi <andreadesimo1998@gmail.com>
2021-12-11 16:25:09 +0100

    main.js fix forse

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-function openNav() {
-  document.getElementById("info").style.display = "grid";
-}
-
-function closeNav() {
-  document.getElementById("info").style.display = "none";
-}
-
 $(document).on('scroll', function(){
     if ( $(window).scrollTop() > 50) {
         $('.header').addClass('active');

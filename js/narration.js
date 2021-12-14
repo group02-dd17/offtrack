16a50c5038e14640918165e9ac73042dfe8e6f53
Raffo98 <95477458+Group02-Density@users.noreply.github.com>
2021-12-14 15:51:26 +0100

    update hash line

--- js/narration.js
+++ js/narration.js
@@ -1,93 +1,111 @@
 let videoList = document.getElementsByClassName("wrapper"); //Array to obtain all the div.wrapper elements
-let flagMute = false; //check volume video
+let flagMute = true; //check volume video
 
 volumeVideos(0.01);
 
-  videoList.forEach(function (item, index) {
-    videoList[index].lastElementChild.volume = 0.01;
-  });
+let lines = function (l) {
+  let xyVertex = []; //Array that will contain the vertexes of the polyLine
 
-  let lines = function (l) {
-    let xyVertex = []; //Array that will contain the vertexes of the polyLine
+  l.setup = function () {
+    // var canvasL = l.createCanvas(l.windowWidth, document.getElementById("container").offsetHeight, l.WEBGL);
+    var canvasL = l.createCanvas(
+      l.windowWidth,
+      document.getElementById("container").offsetHeight
+    );
+    l.pixelDensity(1);
 
-    l.setup = function () {
-      // var canvasL = l.createCanvas(l.windowWidth, document.getElementById("container").offsetHeight, l.WEBGL);
-      var canvasL = l.createCanvas(
-        l.windowWidth,
-        document.getElementById("container").offsetHeight
-      );
-      l.pixelDensity(1);
-      console.log("canva fr 1");
+    canvasL.parent("#canvasLines");
 
-      console.log(l.windowWidth);
-      console.log(document.getElementById("container").offsetHeight);
+    var firstNode = document.getElementById("wrapper-vid1");
+    xyVertex.push({
+      // x: firstNode.lastElementChild.getBoundingClientRect().left + firstNode.offsetWidth / 2 - l.width/2,
+      // y: firstNode.lastElementChild.getBoundingClientRect().top - firstNode.parentNode.getBoundingClientRect().top + firstNode.lastElementChild.offsetHeight / 2 -l.height/2,
 
-      canvasL.parent("#canvasLines");
+      x:
+        firstNode.lastElementChild.getBoundingClientRect().left +
+        firstNode.offsetWidth / 2,
+      y:
+        firstNode.lastElementChild.getBoundingClientRect().top -
+        document.getElementById("title").getBoundingClientRect().top +
+        firstNode.lastElementChild.offsetHeight / 2,
+    });
+  };
 
-      var firstNode = document.getElementById("wrapper-vid1");
-      xyVertex.push({
-        // x: firstNode.lastElementChild.getBoundingClientRect().left + firstNode.offsetWidth / 2 - l.width/2,
-        // y: firstNode.lastElementChild.getBoundingClientRect().top - firstNode.parentNode.getBoundingClientRect().top + firstNode.lastElementChild.offsetHeight / 2 -l.height/2,
+  l.draw = function () {
+    const canvas = document.querySelector("#canvasLines");
+    const ctx = this.canvas.getContext("2d");
 
-        x:
-          firstNode.lastElementChild.getBoundingClientRect().left +
-          firstNode.offsetWidth / 2,
-        y:
-          firstNode.lastElementChild.getBoundingClientRect().top -
-          document.getElementById("title").getBoundingClientRect().top +
-          firstNode.lastElementChild.offsetHeight / 2,
+    // l.background("black"); //to avoid trails
+    l.clear();
+
+    l.drawLines("covid", "mask-quote-3");
+
+    l.noFill();
+    l.stroke("red");
+    l.strokeWeight(2);
+
+    $(videoList).ready(function () {
+      videoList.forEach(function (item, index) {
+        //when :hover the video
+        videoList[index].lastElementChild.addEventListener(
+          "mouseover",
+          function () {
+            //add the center of the video as a new vertex
+            xyVertex.push({
+              x:
+                videoList[index].lastElementChild.getBoundingClientRect().left +
+                videoList[index].offsetWidth / 2,
+              y:
+                videoList[index].lastElementChild.getBoundingClientRect().top -
+                document.getElementById("title").getBoundingClientRect().top +
+                videoList[index].lastElementChild.offsetHeight / 2,
+            });
+          }
+        );
       });
-    };
+    });
 
-    l.draw = function () {
-      const canvas = document.querySelector("#canvasLines");
-      const ctx = this.canvas.getContext("2d");
-
-      // l.background("black"); //to avoid trails
-      l.clear();
-
-      l.noFill();
-      l.stroke("red");
-      l.strokeWeight(2);
-
-      $(videoList).ready(function () {
-        videoList.forEach(function (item, index) {
-          //when :hover the video
-          videoList[index].lastElementChild.addEventListener(
-            "mouseover",
-            function () {
-              //add the center of the video as a new vertex
-              xyVertex.push({
-                x:
-                  videoList[index].lastElementChild.getBoundingClientRect().left +
-                  videoList[index].offsetWidth / 2,
-                y:
-                  videoList[index].lastElementChild.getBoundingClientRect().top -
-                  document.getElementById("title").getBoundingClientRect().top +
-                  videoList[index].lastElementChild.offsetHeight / 2,
-              });
-            }
-          );
-        });
-      });
-
-      //draw the polyLine
-      l.beginShape();
-      xyVertex.forEach(function (item, index) {
-        l.vertex(xyVertex[index].x, xyVertex[index].y);
-      });
-      l.vertex(l.mouseX, l.mouseY);
-      l.endShape();
-      l.mouseWheel = function (event) {
-        //move the square according to the vertical scroll amount
-        l.mouseY += event.delta;
-        //uncomment to block page scrolling
-        //return false;
-      };
+    //draw the polyLine
+    l.beginShape();
+    xyVertex.forEach(function (item, index) {
+      l.vertex(xyVertex[index].x, xyVertex[index].y);
+    });
+    l.vertex(l.mouseX, l.mouseY);
+    l.endShape();
+    l.mouseWheel = function (event) {
+      //move the square according to the vertical scroll amount
+      l.mouseY += event.delta;
+      //uncomment to block page scrolling
+      //return false;
     };
   };
-  let canvasLines = new p5(lines);
 
+  l.drawLines = function (className, idHash) {
+    var vidArray = document.getElementsByClassName(className);
+    var x0 =
+      $("#"+idHash).offset().left +
+      document.getElementById(idHash).offsetWidth / 2;
+    var y0 =
+      $("#"+idHash).offset().top +
+      document.getElementById(idHash).offsetHeight / 2;
+    vidArray.forEach(function (item) {
+      l.beginShape();
+      l.strokeWeight(2);
+      l.noFill();
+      l.stroke("#d8d8d8");
+      l.vertex(x0,y0);
+      l.vertex($(item).offset().left + item.offsetWidth / 2, $(item).offset().top + item.offsetHeight / 2);
+      // l.line(
+      //   x0,
+      //   y0,
+      //   $(item).offset().left + this.offsetWidth / 2,
+      //   $(item).offset().top + this.offsetHeight / 2
+      // );
+      l.endShape();
+    });
+  };
+};
+let canvasLines = new p5(lines);
 
 document.getElementById("toggleVolume").addEventListener("click", function () {
   $(this).find("i").toggleClass("fa-volume-mute");

38819f36b16b0f1df25a0db5393dce925ceba384
Emanuele Ghebaur <emanueleghebaur@gmail.com>
2021-12-16 16:14:01 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,4 +7,4 @@ $(document).on('scroll', function () {
 });
 
 favicon.animate([
-    "/assets/favico/frame_0.png", "/assets/favico/frame_1.png", "/assets/favico/frame_2.png", "/assets/favico/frame_3.png", "/assets/favico/frame_4.png", "/assets/favico/frame_5.png", "/assets/favico/frame_6.png", "/assets/favico/frame_7.png"], 50);
+    "/assets/favico/frame_0.png", "/assets/favico/frame_1.png", "/assets/favico/frame_2.png", "/assets/favico/frame_3.png", "/assets/favico/frame_4.png", "/assets/favico/frame_5.png", "/assets/favico/frame_6.png", "/assets/favico/frame_7.png"], 500);

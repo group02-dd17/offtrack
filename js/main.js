a7047eb13cbfa89084e3629d7715cc13bc2aba68
Emanuele Ghebaur <emanueleghebaur@gmail.com>
2021-12-16 17:13:17 +0100

    favico

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-$(document).on('scroll', function () {
-    if ($(window).scrollTop() > 30) {
-        $('.header').addClass('active');
-    } else {
-        $('.header').removeClass('active');
-    }
+$(document).on("scroll", function () {
+  if ($(window).scrollTop() > 30) {
+    $(".header").addClass("active");
+  } else {
+    $(".header").removeClass("active");
+  }
 });
 
-favicon.animate([
-    "/assets/favico/frame_0.png", "/assets/favico/frame_1.png", "/assets/favico/frame_2.png", "/assets/favico/frame_3.png", "/assets/favico/frame_4.png", "/assets/favico/frame_5.png", "/assets/favico/frame_6.png", "/assets/favico/frame_7.png"], 500);
+favicon.animate(
+  [
+    "/assets/favico/frame_0.png",
+    "/assets/favico/frame_1.png",
+    "/assets/favico/frame_2.png",
+    "/assets/favico/frame_3.png",
+    "/assets/favico/frame_4.png",
+    "/assets/favico/frame_5.png",
+    "/assets/favico/frame_6.png",
+    "/assets/favico/frame_7.png",
+  ],
+  500
+);

0616c92ec3d869549424b1236e40195a29856649
Raffo98 <95477458+Group02-Density@users.noreply.github.com>
2021-12-18 19:19:37 +0100

    update tooltip hash

--- js/main.js
+++ js/main.js
@@ -9,60 +9,6 @@ $(document).on("scroll", function () {
 // TOOLTIP
 const tooltip = document.querySelector("#tooltip");
 
-// $(".tooltip-content").click(function () {
-//   tooltip.classList.add("clicked");
-
-//   switch ($(this).text()) {
-//     case "dark platforms":
-//       tooltip.textContent =
-//         "Digital platforms that are less regulated and moderated hence can be used for hosting content and content creators that may not be tolerated by their more mainstream counterparts.";
-//         break;
-//     case "Bitchute":
-//       tooltip.textContent = "BitChute is a video hosting service launched by Ray Vahey in January 2017. It is known for accommodating far-right individuals and conspiracy theorists, and for hosting hate speech.";
-//       break;
-//     case "misinformation":
-//       tooltip.textContent = "Misinformation is false, inaccurate, or misleading information.";
-//       break;
-//   }
-//   // Pass the button, the tooltip, and some options, and Popper will do the
-//   // magic positioning for you:
-//   Popper.createPopper(this, tooltip, {
-//     placement: "bottom",
-//   });
-// });
-// console.log(document.querySelectorAll('a u'));
-// const instance = tippy(document.querySelectorAll('a u'));
-// console.log(instance);
-
-// instance.setProps ({
-//     placement: 'bottom',
-//     arrow: false,
-//     animation: 'fade',
-//     duration: 500,
-//     trigger: 'click'
-//   });
-
-// $(".tooltip-content").click(function () {
-//   instance.each(function(index, item){
-//     item.setContent(checkTooltip($(this)));
-//   });
-// });
-
-// function checkTooltip(content) {
-//   var text;
-//   switch (content.text()) {
-//     case "dark platforms":
-//       text = "Digital platforms that are less regulated and moderated hence can be used for hosting content and content creators that may not be tolerated by their more mainstream counterparts.";
-//       return text;
-//     case "Bitchute":
-//       text = "BitChute is a video hosting service launched by Ray Vahey in January 2017. It is known for accommodating far-right individuals and conspiracy theorists, and for hosting hate speech.";
-//       return text;
-//     case "misinformation":
-//       text = "Misinformation is false, inaccurate, or misleading information.";
-//       return text;
-//   };
-// }
-
 /* http://mit-license.org */ function e() {
   function f(a) {
     var b = g.createElement("link");
